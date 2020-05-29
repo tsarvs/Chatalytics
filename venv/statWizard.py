4d0267d2e881fe73be743075615fa3389256d188
tsarvs <tfsarv01@louisville.edu>
2020-05-29 00:24:53 -0500

    cleaned up stat wizard

--- venv/statWizard.py
+++ venv/statWizard.py
@@ -1,6 +1,1 @@
-def calculate_timestamp_frequency_days(timestampDict):
-    if(len(timestampDict) <= 0):
-        return
 
-    for key in timestampDict:
-        print(str(key) + "," + str(timestampDict[key]))
